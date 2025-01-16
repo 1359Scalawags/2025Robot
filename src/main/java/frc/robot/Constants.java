4285b39951ec3621a475e295b8daf3d118b830f7
NASA-12-1962 <knoxbeaufort@protonmail.com>
2025-01-15 20:21:20 -0800

    Made motor ID constants (nick)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+
+import com.pathplanner.lib.util.PIDConstants;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +16,60 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+  public static class SwereveSubsystem {
+    /*
+     * Motor ID's for the rev motors on the swerve Drive (in json file), Pheonix motors are set seperatly
+     *-------------------------------------
+     *  SparkMAX:
+     * FRONT LEFT-  1
+     * FRONT RIGHT- 2
+     * BACK LEFT-   3
+     * BACK RIGHT-  4
+     * -------------------------------------
+     * Falcon500:
+     * FRONT LEFT-  1
+     * FRONT RIGHT- 2
+     * BACK LEFT-   3
+     * BACK RIGHT-  4
+     * -------------------------------------
+     */
+
+
+
+  }
+
+  public static class ClimberSubsystem {
+    public static final int kBabyLockingMotorID = 5;
+    public static final int kPositionMotorID = 6;
+
+
+  }  
+
+  public static class ArmSubsystem {
+    public static final int kWristMotorID = 7;
+    public static final int kPullyMotorID = 8;
+    public static final int kReversedScrewMotorID = 8;
+    public static final int kArmMotorID = 9;
+
+
+  }
+
+  public static class Vision {
+
+  }
+
+
   public static class OperatorConstants {
+
+    public static class DriverJoysticks {
+
+    }
+
+    public static class AssistSubsystem {
+
+    }
+
     public static final int kDriverControllerPort = 0;
   }
 }
