84be2d4018ed9f51479cf3a283f512f34d31a69c
NASA-12-1962 <knoxbeaufort@protonmail.com>
2025-01-18 16:01:49 -0800

    added motors to climber

--- src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -1,10 +1,19 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.util.sendable.Sendable;
+import frc.robot.extensions.SendableCANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ClimberSubsystem extends SubsystemBase{
+  private SendableCANSparkMax babyLockingMotor;
+  private SendableCANSparkMax positionMotor;
+
     public ClimberSubsystem() {
-        
+      babyLockingMotor = new SendableCANSparkMax(Constants.ClimberSubsystem.kBabyLockingMotorID, MotorType.kBrushless);
+      positionMotor = new SendableCANSparkMax(Constants.ClimberSubsystem.kPositionMotorID, MotorType.kBrushless);
     }
     @Override
     public void periodic() {
