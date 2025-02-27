70ec9bd621cf11962ff778b588b3a19fed74ef61
mrhelland <tim.helland@lebanon.k12.or.us>
2025-02-27 10:16:35 -0800

    Remove unused import.

--- src/main/java/frc/robot/commands/ArmCommands/MovePulleyWithJoystick.java
+++ src/main/java/frc/robot/commands/ArmCommands/MovePulleyWithJoystick.java
@@ -7,7 +7,6 @@ package frc.robot.commands.ArmCommands;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
 import frc.robot.subsystems.ArmSubsystem;
 
 
