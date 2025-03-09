7e45d3246bc78335eb1bb1585399135d26fb1a89
NASA-12-1962 <knoxbeaufort@protonmail.com>
2025-03-09 13:47:11 -0700

    fixed izone for wrist

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -169,7 +169,7 @@ public final class Constants {
       public static final double kP = 0.006;
       public static final double kI = 0.0000006;
       public static final double kD = 0.006;
-      public static final double kIZone = 0;
+      public static final double kIZone = 2;
       public static final double kGravityFF = 0;
       public static final double kMinGravityFF = 0;
       }
