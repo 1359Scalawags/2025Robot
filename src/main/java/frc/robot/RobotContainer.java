87d591865bcbf7211681cfee7669974d9e04fd94
mrhelland <tim.helland@lebanon.k12.or.us>
2025-03-18 20:04:43 -0700

    Allow arm to be disabled using a flag.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -67,155 +67,176 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  //TODO: we need to intialize the arm.
+  // TODO: we need to intialize the arm.
   // The robot's subsystems and commands are defined here
 
   private final SwerveSubsystem m_SwerveSubsystem = new SwerveSubsystem(
-    new File(Filesystem.getDeployDirectory(), "YAGSLConfigJSON/Pearl"));
-    // TODO: This needs to be enabled when ready. Also fix initializeArm() below and uncomment calls in Robot.java
-  private final ArmSubsystem m_ArmSubsystem = new ArmSubsystem();
-   private final ClimberSubsystem m_ClimberSubsystem;
-  //  private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
-   private final CommandJoystick m_DriverJoystick = new CommandJoystick(Constants.Operator.DriverJoystick.kPort);
-   private final CommandJoystick m_AssistantJoystick = new CommandJoystick(Constants.Operator.AssistJoystick.kPort);
+      new File(Filesystem.getDeployDirectory(), "YAGSLConfigJSON/Pearl"));
+  // TODO: This needs to be enabled when ready. Also fix initializeArm() below and
+  // uncomment calls in Robot.java
+  private final ArmSubsystem m_ArmSubsystem;
+  private final ClimberSubsystem m_ClimberSubsystem;
+  // private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
+  private final CommandJoystick m_DriverJoystick = new CommandJoystick(Constants.Operator.DriverJoystick.kPort);
+  private final CommandJoystick m_AssistantJoystick = new CommandJoystick(Constants.Operator.AssistJoystick.kPort);
 
   SendableChooser<Command> autoChooser;
-  SendableChooser<Command> pipelineChooser;  
+  SendableChooser<Command> pipelineChooser;
 
-
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    if(Constants.ClimberSubsystem.kEnabled) {
+    if (Constants.ClimberSubsystem.kEnabled) {
       m_ClimberSubsystem = new ClimberSubsystem();
     } else {
       m_ClimberSubsystem = null;
     }
-    autoChooser = AutoBuilder.buildAutoChooser(); //This will populate all the autos in the project.
+    if (Constants.ArmSubsystem.kEnabled) {
+      m_ArmSubsystem = new ArmSubsystem();
+    } else {
+      m_ArmSubsystem = null;
+    }
+    autoChooser = AutoBuilder.buildAutoChooser(); // This will populate all the autos in the project.
     pipelineChooser = new SendableChooser<Command>();
 
-    if(Constants.kDebug) {
+    if (Constants.kDebug) {
       SmartDashboard.putData("Pipeline Chooser", pipelineChooser);
-      SmartDashboard.putData("Auto Chooser ", autoChooser);      
+      SmartDashboard.putData("Auto Chooser ", autoChooser);
     }
-
 
     // Configure the trigger bindings
     configureBindings();
-    setDefaultCommands();     
+    setDefaultCommands();
   }
 
   private void setDefaultCommands() {
     m_SwerveSubsystem.setDefaultCommand(
-      new AbsoluteFieldDrive(m_SwerveSubsystem,
-      this::driverGetForward,
-      this::driverGetRight,
-      this::driverGetZ,
-      this::driverGetThrottle,
-      m_SwerveSubsystem::getFeildCentric, 
-      false
-      ));
+        new AbsoluteFieldDrive(m_SwerveSubsystem,
+            this::driverGetForward,
+            this::driverGetRight,
+            this::driverGetZ,
+            this::driverGetThrottle,
+            m_SwerveSubsystem::getFeildCentric,
+            false));
 
-      if(m_ClimberSubsystem != null) {
-        m_ClimberSubsystem.setDefaultCommand(
-          new MoveClimber(m_ClimberSubsystem, 
-          this::assistantGetY));        
-      }
-
-
-      // This is only for testing the pulley motor directly. It will interact badly with the climber when it is unlocked
-      // m_ArmSubsystem.setDefaultCommand(new MovePulleyWithJoystick(m_ArmSubsystem, this::assistantGetY));
-
+    if (m_ClimberSubsystem != null) {
+      m_ClimberSubsystem.setDefaultCommand(
+          new MoveClimber(m_ClimberSubsystem,
+              this::assistantGetY));
     }
 
-      
+    // This is only for testing the pulley motor directly. It will interact badly
+    // with the climber when it is unlocked
+    // m_ArmSubsystem.setDefaultCommand(new MovePulleyWithJoystick(m_ArmSubsystem,
+    // this::assistantGetY));
 
-      //TODO: Are deadbands implemented for joysticks?
-      // Configure remote movements
-    public double assistantGetY() {
-      return -m_AssistantJoystick.getY();
-    }
-    public double assistantGetX() {
-      return -m_AssistantJoystick.getX();
-    }
-    public double assistantGetZ() {
-      return -m_AssistantJoystick.getZ();
-    }
-    public double driverGetRight() {
-      return -m_DriverJoystick.getX();
-    }
-    public double driverGetForward() {
-      return -m_DriverJoystick.getY();
-    }
-    public double driverGetZ() {
-      return m_DriverJoystick.getZ();
-    }
-    public double driverGetThrottle() {
-      return m_DriverJoystick.getThrottle();
-    }
+  }
+
+  // TODO: Are deadbands implemented for joysticks?
+  // Configure remote movements
+  public double assistantGetY() {
+    return -m_AssistantJoystick.getY();
+  }
+
+  public double assistantGetX() {
+    return -m_AssistantJoystick.getX();
+  }
+
+  public double assistantGetZ() {
+    return -m_AssistantJoystick.getZ();
+  }
+
+  public double driverGetRight() {
+    return -m_DriverJoystick.getX();
+  }
+
+  public double driverGetForward() {
+    return -m_DriverJoystick.getY();
+  }
+
+  public double driverGetZ() {
+    return m_DriverJoystick.getZ();
+  }
+
+  public double driverGetThrottle() {
+    return m_DriverJoystick.getThrottle();
+  }
 
   public Command getAutonomousCommand() {
     return autoChooser.getSelected();
   }
 
-
-
-
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-        // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //new Trigger(m_exampleSubsystem::exampleCondition)
-    //    .onTrue(new ExampleCommand(m_exampleSubsystem));
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    // .onTrue(new ExampleCommand(m_exampleSubsystem));
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
+    // pressed,
     // cancelling on release.
-  //  m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
 
-  // new JoystickButton(assistantJoystick, Constants.Operator.AssistJoystick.deployClimberButton)
-  //     .onTrue(new DeployClimber(m_ClimberSubsystem));
-  // }
+    // new JoystickButton(assistantJoystick,
+    // Constants.Operator.AssistJoystick.deployClimberButton)
+    // .onTrue(new DeployClimber(m_ClimberSubsystem));
+    // }
 
-    //Bindin Arm Commands
-  m_AssistantJoystick.button(1).onFalse(new closeClawCommand(m_ArmSubsystem));
-  m_AssistantJoystick.button(1).onTrue(new openClawCommand(m_ArmSubsystem));
+    // Bindin Arm Commands
+    if(m_ArmSubsystem != null) {
+      m_AssistantJoystick.button(1).onFalse(new closeClawCommand(m_ArmSubsystem));
+      m_AssistantJoystick.button(1).onTrue(new openClawCommand(m_ArmSubsystem));
 
-  m_AssistantJoystick.button(8).onTrue(new goToHeightHumanStation(m_ArmSubsystem));
-  m_AssistantJoystick.button(10).onTrue(new goToHeightLevelFour(m_ArmSubsystem));
-  m_AssistantJoystick.button(7).onTrue(new goToHeightLevelThree(m_ArmSubsystem));
-  m_AssistantJoystick.button(6).onTrue(new goToHeightLevelTwo(m_ArmSubsystem));
-  m_AssistantJoystick.button(9).onTrue(new goToHightGround(m_ArmSubsystem));
+      m_AssistantJoystick.button(8).onTrue(new goToHeightHumanStation(m_ArmSubsystem));
+      m_AssistantJoystick.button(10).onTrue(new goToHeightLevelFour(m_ArmSubsystem));
+      m_AssistantJoystick.button(7).onTrue(new goToHeightLevelThree(m_ArmSubsystem));
+      m_AssistantJoystick.button(6).onTrue(new goToHeightLevelTwo(m_ArmSubsystem));
+      m_AssistantJoystick.button(9).onTrue(new goToHightGround(m_ArmSubsystem));      
+    }
 
-  //Binding Climber Commands
-      //can we make this simpler (sequential command)?
-          //proposed solution
-            //if (useClimber == false) {
-            // - 
-            // - insert all ARM commands
-            // - insert the deufault command for climber movement
-            //} else {
-            // - all of the CLIMBER commands
-            // - climber default movment command
-            //}
-            // then just make a button that flips that variable. talk to drive team about it.
 
-    if(m_ClimberSubsystem != null) {
+    // Binding Climber Commands
+    // can we make this simpler (sequential command)?
+    // proposed solution
+    // if (useClimber == false) {
+    // -
+    // - insert all ARM commands
+    // - insert the deufault command for climber movement
+    // } else {
+    // - all of the CLIMBER commands
+    // - climber default movment command
+    // }
+    // then just make a button that flips that variable. talk to drive team about
+    // it.
+
+    if (m_ClimberSubsystem != null) {
       m_AssistantJoystick.button(16).onTrue(new UnlockClimberSubsystem(m_ClimberSubsystem));
 
-      
-      ////m_AssistantJoystick.button(14).onTrue(new RetractClimber(m_ClimberSubsystem));
+      //// m_AssistantJoystick.button(14).onTrue(new
+      //// RetractClimber(m_ClimberSubsystem));
 
       m_AssistantJoystick.button(15).onTrue(new LockClimberBar(m_ClimberSubsystem));
       m_AssistantJoystick.button(4).onTrue(new UnLockClimberBar(m_ClimberSubsystem));
@@ -226,91 +247,102 @@ public class RobotContainer {
       m_AssistantJoystick.button(13).onTrue(new LockingPosition(m_ClimberSubsystem));
       m_AssistantJoystick.button(11).onTrue(new LockedPosition(m_ClimberSubsystem));
 
-      m_AssistantJoystick.button(3).onTrue(new DeployClimber(m_ClimberSubsystem));      
+      m_AssistantJoystick.button(3).onTrue(new DeployClimber(m_ClimberSubsystem));
     }
 
+    // //TODO: Make a sequential command to lock the arm motor.
 
-  // //TODO: Make a sequential command to lock the arm motor.
-
-    // m_AssistantJoystick.button(1).onTrue(Commands.sequence(new LockingPosition(m_ClimberSubsystem),
-    //   new LatchServo(m_ClimberSubsystem),
-    //   new WaitCommand(1.5),
-    //   new LockedPosition(m_ClimberSubsystem)));
-
+    // m_AssistantJoystick.button(1).onTrue(Commands.sequence(new
+    // LockingPosition(m_ClimberSubsystem),
+    // new LatchServo(m_ClimberSubsystem),
+    // new WaitCommand(1.5),
+    // new LockedPosition(m_ClimberSubsystem)));
 
     m_DriverJoystick.button(1).onTrue(new ZeroGyroCommand(m_SwerveSubsystem));
     m_DriverJoystick.button(2).onTrue(new FieldCentricCommand(m_SwerveSubsystem));
     m_DriverJoystick.button(3).onTrue(new RobotCentricCommand(m_SwerveSubsystem));
 
-  // if (Constants.kDebug) {
-  //   new JoystickButton(m_DriverJoystick, Constants.Operator.DriverJoystick.driveForwardButton)
-  //     .onTrue(new DriveForwardCommand(m_SwerveSubsystem));
+    // if (Constants.kDebug) {
+    // new JoystickButton(m_DriverJoystick,
+    // Constants.Operator.DriverJoystick.driveForwardButton)
+    // .onTrue(new DriveForwardCommand(m_SwerveSubsystem));
 
-  //   new JoystickButton(m_DriverJoystick, Constants.Operator.DriverJoystick.driveRightButton)
-  //     .onTrue(new DriveRightCommand(m_SwerveSubsystem));
+    // new JoystickButton(m_DriverJoystick,
+    // Constants.Operator.DriverJoystick.driveRightButton)
+    // .onTrue(new DriveRightCommand(m_SwerveSubsystem));
 
-  //   new JoystickButton(m_DriverJoystick, Constants.Operator.DriverJoystick.rotateCCWButton)
-  //     .onTrue(new RotateCCWCommand(m_SwerveSubsystem));    
-  //   }
-  //}
+    // new JoystickButton(m_DriverJoystick,
+    // Constants.Operator.DriverJoystick.rotateCCWButton)
+    // .onTrue(new RotateCCWCommand(m_SwerveSubsystem));
+    // }
+    // }
 
-  //XXX: These are for testing only. They should be commented after testing is completed.
-  m_DriverJoystick.button(12).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.N));
-  m_DriverJoystick.button(15).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.S));
-  m_DriverJoystick.button(16).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.E));
-  m_DriverJoystick.button(13).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.W));
-  m_DriverJoystick.button(8).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.SE));
-  m_DriverJoystick.button(11).whileTrue(new Rotate(m_SwerveSubsystem, RotateDirection.CW));
-  m_DriverJoystick.button(5).whileTrue(new Rotate(m_SwerveSubsystem, RotateDirection.CCW));
+    // XXX: These are for testing only. They should be commented after testing is
+    // completed.
+    m_DriverJoystick.button(12).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.N));
+    m_DriverJoystick.button(15).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.S));
+    m_DriverJoystick.button(16).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.E));
+    m_DriverJoystick.button(13).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.W));
+    m_DriverJoystick.button(8).whileTrue(new MoveCardinal(m_SwerveSubsystem, CardinalDirection.SE));
+    m_DriverJoystick.button(11).whileTrue(new Rotate(m_SwerveSubsystem, RotateDirection.CW));
+    m_DriverJoystick.button(5).whileTrue(new Rotate(m_SwerveSubsystem, RotateDirection.CCW));
 
   }
 
   public Command lockClimberSubsystemWhenDisabled() {
-    if(m_ClimberSubsystem == null)
-      return new WaitCommand(0.01);
+    if (m_ClimberSubsystem == null) {
+      return new WaitCommand(0.01);      
+    }
     return new LockClimberSubsystem(m_ClimberSubsystem);
   }
 
   public Command initializeClimberEncoders() {
-    if(m_ClimberSubsystem == null)
-      return new WaitCommand(0.01);
+    if (m_ClimberSubsystem == null) {
+      return new WaitCommand(0.01);      
+    }
     return new InitilizeClimberEncoders(m_ClimberSubsystem).ignoringDisable(true);
   }
 
   public Command initializeClimberPosition() {
-    if(m_ClimberSubsystem == null)
-      return new WaitCommand(0.01);
+    if (m_ClimberSubsystem == null) {
+      return new WaitCommand(0.01);      
+    }
     return new RetractClimber(m_ClimberSubsystem);
   }
 
   public Command intializeArmEncoders() {
+    if(m_ArmSubsystem == null) {
+      return new WaitCommand(0.01);  
+    }
     Command initializeArmEncoders = new InitilizeArmEncoders(m_ArmSubsystem);
     return initializeArmEncoders.ignoringDisable(true);
   }
 
   public Command intializeTheArm() {
+    if(m_ArmSubsystem == null) {
+      return new WaitCommand(0.01);  
+    }
     Command zeroClaw = new ZeroClaw(m_ArmSubsystem);
     Command zeroPulley = new ZeroPulley(m_ArmSubsystem);
     Command homePulley = new goToHeightHome(m_ArmSubsystem);
 
-  //   return homePulley;
+    // return homePulley;
 
-   return Commands.sequence(zeroPulley, zeroClaw, homePulley );
+    return Commands.sequence(zeroPulley, zeroClaw, homePulley);
     // return initializeArm.Commands.sequence(homeClaw.andThen(homePulley));
   }
 
   // public Command intializeJustTheArm() {
-  //   return new InitilizeArm(m_ArmSubsystem);
+  // return new InitilizeArm(m_ArmSubsystem);
   // }
 
   // public Command homeTheClaw() {
-  //   return new HomeTheClaw(m_ArmSubsystem);
+  // return new HomeTheClaw(m_ArmSubsystem);
   // }
 
   // public Command homeThePulley() {
-  //   return new HomeThePulley(m_ArmSubsystem);
+  // return new HomeThePulley(m_ArmSubsystem);
   // }
- 
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
